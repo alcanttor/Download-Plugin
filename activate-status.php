90f4c3d15757a78e32dfbef482186a463bda7796
mJawed <mohd.jawed@alcanttor.com>
2019-09-17 14:42:53 +0530

    CSS class fixes

--- activate-status.php
+++ activate-status.php
@@ -2,7 +2,7 @@
 $dpwapObj = new dpwapuploader();
 if($_GET['page']=="activate-status") { 
 if(isset($_POST['dpid']) && $_POST['dpid']>0){  ?>
-<div id="sm_status" class="wrap pc-wrap">
+<div id="sm_status" class="wrap pc-wrap dpwap-box-wrap">
 	 <div id="mpiblock">
 		<div class="postbox">
 				<h3 class="hndle"><span><?php _e('Activation Status','dpwap'); ?></span></h3>
